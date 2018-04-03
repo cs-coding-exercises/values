61fc6d7f2d666e7a403ed019d7ca62a5dad5c483
cs-coding-exercises <37808365+cs-coding-exercises@users.noreply.github.com>
2018-04-03 16:41:47 -0400

    Update values.rb

--- values.rb
+++ values.rb
@@ -1,55 +1,61 @@
 require 'cucumber'
+require 'value_page_object.rb'
 
+@vp = ValuesPage. new
+
+#----------------------------------------------------------------
 Given(/^I have three values$/) do |arg|
 	values = "#{arg}"
 end
 
 When(/^user navigates to values page$/) do
-	visit_values_page
+	@vp.visit_values_page
 end
 
 Then(/^Three values should appear$/) do |values|
-	verify_values_fields(#{values})
+	@vp.verify_values_fields(#{values})
 end	
 
 #----------------------------------------------------------------
-Given(/^negative, zero and positive numbers$/)  do |arg|
+Given(/^values including zero and negative$/)  do |arg|
 	values = "#{arg}"
 end
 
 When(/^user navigates to values page$/) do
-	visit_values_page
+	@vp.visit_values_page
 end
 
 Then(/^No zero or negative values should appear on the page$/) do |values|
-	verify_values_page(#{values})
+	@vp.verify_values_page(#{values})
 end
 
-
 #------------------------------------------------------------
-Given(/^negative, zero and positive values$/) do |arg1|
-	values = "#{arg1}"		
+Given(/^one or more positive values/) do |arg1|
+	values = "#{arg1}"
+	total_of_values = @vp.get_sum_of_values(values)
 end
 
 When(/^user navigates to values page$/) do
-	visit_values_page
+	@vp.visit_values_page
 end
 
-Then(/^the Total Value texts field should equal "(.*?)"$/) do |arg2|
-	verify_total("#{arg2}")
+Then(/^the Total Value text field should equal the sum of the Values$/) do
+	total_f = @vp.get_total_amount
+	@vp.values_equal_total(total_of_values, total_f)
 end
 
 
 #----------------------------------------------------------------
-Given(/^values of different amounts before and after the decimal place$/) do |arg|
+Given(/^values of different significant digits$/) do |arg|
 	values = "{#arg1}"
+	total_of_values = @vp.get_sum_of_values(values)
 end
 
 When(/^User navigates to values page$/) do
-	visit_values_page
+	@vp.visit_values_page
 end
 
-Then(/^All values should appear as formatted currency$/) do |arg1,arg2|
-	verify_values_fields("#{arg1}")	
-	verify_total("#{arg2}")
+Then(/^All values should appear as formatted currency$/) do
+	@vp.verify_values_fields("#{arg1}")
+	@vp.verify_total_fields(total_of_values)
 end
